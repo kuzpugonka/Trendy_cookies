a417623313467fa92d1637c10a90c46482e4b5ce
kuzpugonka <6599255@bk.ru>
2024-07-19 15:31:58 +0300

    конечный вариант dz6 до деплоя

---
+++ src/app/app.component.ts
@@ -0,0 +1,88 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  currency = '$';
+
+  productsData: any;
+
+  form = this.fb.group({
+    product: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://testologia.ru/cookies')
+      .subscribe((data) => (this.productsData = data));
+  }
+
+  scrollTo(target: HTMLElement, product?: any) {
+    target.scrollIntoView({ behavior: 'smooth' });
+    if (product) {
+      this.form.patchValue({
+        product:
+          product.title + ' (' + product.price + ' ' + this.currency + ')',
+      });
+    }
+  }
+
+  switchSugarFree(e: any) {
+    this.http
+      .get(
+        'https://testologia.ru/cookies' +
+          (e.currentTarget.checked ? '?sugarfree' : '')
+      )
+      .subscribe((data) => (this.productsData = data));
+  }
+
+  changeCurrency() {
+    let newCurrency = '$';
+    let coefficient = 1;
+
+    if (this.currency === '$') {
+      newCurrency = '₽';
+      coefficient = 9;
+    } else if (this.currency === '₽') {
+      newCurrency = 'BYN';
+      coefficient = 3;
+    } else if (this.currency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    }
+
+    this.currency = newCurrency;
+
+    this.productsData.forEach((item: any) => {
+      item.price = +(item.basePrice * coefficient).toFixed(1);
+    });
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+      this.http
+        .post('https://testologia.ru/cookies-order', this.form.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message);
+            this.form.reset();
+          },
+          error: (response: any) => {
+            alert(response.error.message);
+          },
+        });
+    }
+  }
+}
